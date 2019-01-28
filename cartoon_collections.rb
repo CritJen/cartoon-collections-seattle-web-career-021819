a4a282c803c0a0c414888e7607987e0211c6424c
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-28 07:59:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,9 @@ end
 
 def long_planeteer_calls(calls)
  calls.any?{|call| call.length >= 4}
- false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
+  cheeses.find{|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
